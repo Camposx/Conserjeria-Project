0dd214cfa1d2e84d8a6e65f238a10f2fd13d8b60
Camposx <alejandrocamposgon@gmail.com>
2024-11-12 16:03:47 +0100

    feat: add foreach to table for problems

--- src/Views/callList.php
+++ src/Views/callList.php
@@ -6,7 +6,29 @@
         require_once("Components/header.php");
     ?>
     <main>
-
+    <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Room</th>
+                    <th scope="col">Issue</th>
+                    <th scope="col">Date Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($data["call"] as $call) {
+                        echo "
+                            <tr>
+                                <th>{$call->id}</th>
+                                <td>{$call->room}</td>
+                                <td>{$call->issue}</td>
+                                <td>{$call->dateTime}</td>
+                            </tr>";
+                    }
+                ?>                
+            </tbody>
+        </table>
     </main>
     <?php
     require_once("Components/footer.php");
